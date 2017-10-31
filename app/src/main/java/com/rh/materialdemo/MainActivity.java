1ae322697aaa22be43df3ccdd54343c3eff2a732
RH <swd-004@mail.foxconn.com>
2017-10-31 20:27:27 +0800

    添加滑动菜单 DrawerLayout NavigationView

--- app/src/main/java/com/rh/materialdemo/MainActivity.java
+++ app/src/main/java/com/rh/materialdemo/MainActivity.java
@@ -1,5 +1,10 @@
 package com.rh.materialdemo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +13,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +21,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//设置toolbar替代原ActionBar
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);//显示导航按钮
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);//设置导航按钮图标，默认为一个返回箭头
+        }
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setCheckedItem(R.id.nav_call);//将Call菜单项设置为默认选中
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar,menu);
+        getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.camera:
-                Toast.makeText(this,"You clicked camera",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked camera", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.backup:
-                Toast.makeText(this,"You clicked backup",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked backup", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.sunny:
-                Toast.makeText(this,"You clicked sunny",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "You clicked sunny", Toast.LENGTH_SHORT).show();
+                break;
+            //HomeAsUp按钮的id永远是android.R.id.home
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
