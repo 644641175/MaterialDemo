f5c795d524885973fe00087aa08d66c8b9c87158
RH <swd-004@mail.foxconn.com>
2017-11-02 19:53:24 +0800

    悬浮按钮+Snackbar+CoordinatorLayout

--- app/src/main/java/com/rh/materialdemo/MainActivity.java
+++ app/src/main/java/com/rh/materialdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rh.materialdemo;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +30,25 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);//显示导航按钮
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);//设置导航按钮图标，默认为一个返回箭头
         }
-        initNavigationView();
+        initNavigationView();//初始化滑动菜单布局
+        initFloatingActionButton();//初始化悬浮按钮
+    }
+
+    private void initFloatingActionButton() {
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Snackbar,可添加功能的Toast
+                Snackbar.make(v, "Data deleted", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"Data restored",Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
     }
 
     private void initNavigationView() {
