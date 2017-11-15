0d4e4420df5f260b3fdd087632c796a46e40ab36
RH <swd-004@mail.foxconn.com>
2017-11-15 16:17:46 +0800

    创建数据库和表
    使用开源库LitePal来创建和管理数据库

--- app/src/main/java/com/rh/materialdemo/MyApplication.java
+++ app/src/main/java/com/rh/materialdemo/MyApplication.java
@@ -3,6 +3,8 @@ package com.rh.materialdemo;
 import android.app.Application;
 import android.content.Context;
 
+import org.litepal.LitePalApplication;
+
 /**
  * Created by RH on 2017/11/4.
  */
@@ -13,6 +15,15 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        /**LitePalApplication.initialize(context)的调用原则是尽可能早，
+        *比如合适的调用位置是在Application的onCreate()里调用。
+        *调用时传递的参数是Application的context，
+        *不要使用任何activity或service的实例作为参数，否则可能发生内存泄漏。
+         *
+         * 也可以在AndroidManifest.xml，配置application的参数android:name="org.litepal.LitePalApplication"
+         * 然后MyApplication继承LitePalApplication
+         */
+        LitePalApplication.initialize(context);
     }
 
     public static Context getContext(){
---
+++ app/src/main/java/com/rh/materialdemo/db/City.java
@@ -0,0 +1,46 @@
+package com.rh.materialdemo.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by RH on 2017/11/15.
+ */
+//对数据库进行CRUD（增、删、改）操作时必须要继承DataSupport
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
